e981fb7599ea10bec5b7b0f0efafa55d4f0fb581
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-06-07 23:45:23 +0900

    feat : text count function progressing

--- script.js
+++ script.js
@@ -5,47 +5,59 @@ let certifiNumber = 0;
 let introNumber = 0;
 let interviewNumber = 0;
 
+const textCount = () => {
+  document.querySelector(".count").innerHTML = document.querySelector(
+    ".certifiText"
+  ).value.length;
+  console.log(document.querySelector(".certifiText").value.length);
+};
+
 const handleAddCertifi = () => {
   let div = document.createElement("div");
   certifiNumber += 1;
-  div.innerHTML = `<div class="certifiContent">
+  div.innerHTML = `
      <div>
      <span class="certifiNumber">${certifiNumber}</span>
      <span>. </span>
       <input type="text"/>
      </div>
-      <input type="textarea" class="certifiText"/>
-  <div>`;
-
+      <textarea class="certifiText" onkeyup="textCount()" ></textarea>
+      <div class="count">0</div>
+  `;
+  div.classList.add("certifiContent");
   certifiContents.appendChild(div);
 };
 
 const handleAddIntro = () => {
   let div = document.createElement("div");
   introNumber += 1;
-  div.innerHTML = `<div class="introContent">
+  div.innerHTML = `
      <div>
      <span class="certifiNumber">${introNumber}</span>
      <span>. </span>
       <input type="text"/>
      </div>
-      <input type="textarea" class="introText"/>
-  <div>`;
-
+      <textarea class="introText" >
+      </textarea>
+      <div class="count">0</div>
+      
+  `;
+  div.classList.add("introContent");
   introContents.appendChild(div);
 };
 
 const handleAddInterview = () => {
   let div = document.createElement("div");
   interviewNumber += 1;
-  div.innerHTML = `<div class="interviewContent">
+  div.innerHTML = `
      <div>
      <span class="interviewNumber">${interviewNumber}</span>
      <span>. </span>
       <input type="text"/>
      </div>
       <input type="textarea" class="interviewText"/>
-  <div>`;
-
+      <div class="count">0</div>
+  `;
+  div.classList.add("interviewContent");
   interviewContents.appendChild(div);
 };
